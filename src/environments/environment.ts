2c4a153c9ee7549e682887ce50912d6112a3d268
Anton Halauko <anton.halauko.main@gmail.com>
2024-04-21 21:09:54 +0300

    update import route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://1xxd1gp04k.execute-api.eu-central-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://6rs5ppxmd0.execute-api.eu-central-1.amazonaws.com',
+    import: 'https://mlgjhpe2j5.execute-api.eu-central-1.amazonaws.com',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
